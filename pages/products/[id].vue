04c8df21130831ef0670e66dfb38e6e79c818405
Dan Carlier <dan.carlier@gmail.com>
2023-03-20 18:45:06 +1100

    Add product page accessible via route parameter

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quas incidunt odio ipsa explicabo, repellat porro numquam hic fugit eligendi.</p>
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
