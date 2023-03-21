ff3fdab1617579ec0685ed8b6c65947b66e49e94
Dan Carlier <dan.carlier@gmail.com>
2023-03-21 12:07:34 +1100

    cDCreate default and products layouts

---
+++ layouts/products.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <NuxtLink to="/products">Products</NuxtLink>
+        <ul>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- output the page content -->
+    <div>
+      <slot />
+    </div>
+    <footer>
+      <li>
+        <NuxtLink to="/">Home</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/products">Products</NuxtLink>
+      </li>
+    </footer>
+  </div>
+</template>
+
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
